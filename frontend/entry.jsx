7295afe323bc305678e4812ce0d4ee6c75dfa6a1
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-10-28 14:45:33 -0700

    Add session_api_util. Ajax sign-in login and logout. Set up Store, SessionReducer and SessionMiddleware

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
+  window.store = configureStore();
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
